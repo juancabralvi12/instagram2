3a4877a36b5f8759fd9205302a5cff89ddf39177
Juan Cabral <juancabralvi@gmail.com>
2019-10-12 02:36:08 -0500

    Initial Commit

---
+++ lasttest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lasttest
+//
+//  Created by Juan Cabral on 12/10/19.
+//  Copyright © 2019 Juan Cabral. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
