b65de0831f8472cf582cbe34cd488c3ee2c8a070
Juan Cabral <juancabralvi@gmail.com>
2019-11-02 11:34:03 -0600

    Add assets

--- lasttest/SceneDelegate.swift
+++ lasttest/SceneDelegate.swift
@@ -27,7 +27,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             let window = UIWindow(windowScene: windowScene)
             //window.rootViewController = UIHostingController(rootView: contentView)
             
-            window.rootViewController = UINavigationController(rootViewController: FeedController(collectionViewLayout: UICollectionViewFlowLayout()))
+            window.rootViewController = MainTabBarController() /*UINavigationController(rootViewController: FeedController(collectionViewLayout: UICollectionViewFlowLayout()))*/
             self.window = window
             window.makeKeyAndVisible()
         }
--- lasttest/bootstrap/MainTabBarController.swift
+++ lasttest/bootstrap/MainTabBarController.swift
@@ -14,16 +14,38 @@ class MainTabBarController : UITabBarController, UITabBarControllerDelegate {
         super.viewDidLoad()
         delegate = self
         setupViewControllers()
+        self.selectedIndex = 0
+    }
+     
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        let index = viewControllers?.index(of: viewController)
+        if index == 2 {
+            let photoController = UINavigationController(rootViewController  :UIViewController())
+            photoController.view.backgroundColor = .blue
+            present(photoController, animated: true, completion: nil)
+            return false
+        }
+        return true
     }
     
     fileprivate func setupViewControllers() -> Void {
+        
+        
+        let feedController = UINavigationController(rootViewController: FeedController(collectionViewLayout: UICollectionViewFlowLayout()))
+        feedController.tabBarItem.image = UIImage(named: "home_unselected")
+        feedController.tabBarItem.selectedImage = UIImage(named: "home_selected")
+        
+        
         let redViewController = UIViewController()
         redViewController.view.backgroundColor = .red
+        redViewController.tabBarItem.image = UIImage(named:"profile_unselected" )
+        redViewController.tabBarItem.selectedImage = UIImage(named: "profile_selected")
+        
         
         let greenViewController = UIViewController()
         greenViewController.view.backgroundColor = UIColor.green
         
-        viewControllers = [redViewController, greenViewController]
+        viewControllers = [feedController, redViewController, greenViewController]
         tabBar.tintColor = .black
         
         guard let items = tabBar.items else { return }
@@ -31,5 +53,9 @@ class MainTabBarController : UITabBarController, UITabBarControllerDelegate {
         for item in items {
             item.imageInsets = UIEdgeInsets(top: 4, left: 0, bottom: -4, right: 0)
         }
+        
+        //tabBarController?.selectedIndex = 2
+        //tabBarController?.selectedViewController = redViewController
+        //tabBar.selectedItem = tabBar.items![1] as UITabBarItem
     }
 }
